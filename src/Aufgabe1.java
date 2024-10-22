2dc112512507dd0b1a11b3938ffde4a05089ac35
LupDani03 <daniel.lup@stud.ubbcluj.ro>
2024-10-22 15:35:01 +0300

    finished Aufgabe1

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -2,25 +2,86 @@ import java.util.Arrays;
 
 public class Aufgabe1 {
 
-    static int[] nichtAusreichenderNoten(int[] Noten){
-        int[] nichtAusreichender = new int[Noten.length];
+    //1
+    public static int[] nichtAusreichenderNoten(int[] Noten){
+        int[] nichtAusreichender=new int[Noten.length];
         int nALength=0;
         for (int i = 0; i < Noten.length; i++)
-            if(Noten[i]<38)
+            if(Noten[i]<40)
                 nichtAusreichender[nALength++] = Noten[i];
-        nALength--;
         return nichtAusreichender;
     }
 
+    //2
+    public static float durschnittsWert(int[] Noten){
+        float average=0f;
+        for(int i=0; i<Noten.length; i++)
+            average+=Noten[i];
+        average/=Noten.length;
+        return average;
+    }
+
+    //3
+    public static int[] notenRunden(int[] Noten){
+        int[] notenRunden = new int[Noten.length];
+        for(int i=0; i<Noten.length; i++)
+            if(Noten[i]>37)
+                if(Noten[i]%5>2)
+                    notenRunden[i]=Noten[i]+(5-Noten[i]%5);
+                else
+                    notenRunden[i]=Noten[i]-Noten[i]%5;
+            else
+                notenRunden[i]=Noten[i];
+        return notenRunden;
+    }
+
+    //4
+    public static int maximalNote(int[] Noten){
+        int max=0;
+        for(int i=0; i<Noten.length; i++)
+            if(Noten[i]>max)
+                max=Noten[i];
+        return max;
+    }
+
     public static void main(String[] args) {
-        int[] Noten={21,47,50,65,30,99,87};
-        int actualLength=0;
+        int[] Noten={21,47,50,66,12,38,37,87,73};
+        System.out.println("Die Noten Array ist: ");
+        for(int i=0; i<Noten.length; i++)
+            System.out.print(Noten[i]+" ");
+        System.out.println();
+        System.out.println();
+
+        //1
         int[] nichtAusreichenderNoten=nichtAusreichenderNoten(Noten);
+        int actualLength=0;
         for(int i=0; i<nichtAusreichenderNoten.length; i++)
             if(nichtAusreichenderNoten[i]>0)
                 actualLength++;
-        System.out.println("Die nicht Ausreichender Noten sind:");
+        System.out.println("Die nicht Ausreichender Noten sind: ");
         for(int i=0;i<actualLength;i++)
             System.out.print(nichtAusreichenderNoten[i]+" ");
+        System.out.println();
+        System.out.println();
+
+        //2
+        float average=durschnittsWert(Noten);
+        System.out.println("Die Durschnittwert des Notens ist: " + average);
+        System.out.println();
+
+        //3
+        int[] gerundeteNoten=notenRunden(Noten);
+        System.out.println("Die gerundete Noten sind:");
+        for(int i=0;i<gerundeteNoten.length;i++)
+            System.out.print(gerundeteNoten[i]+" ");
+        System.out.println();
+        System.out.println();
+        /*for(int i=0; i<Noten.length; i++)
+            System.out.print(Noten[i]+" ");*/
+
+        //4
+        int maxi=maximalNote(gerundeteNoten);
+        System.out.println("Die maximale Note nach der Rundung ist: "+maxi);
+        System.out.println();
     }
 }
