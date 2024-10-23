9328c275fe49bf8ba40744eca66512b007e497be
LupDani03 <daniel.lup@stud.ubbcluj.ro>
2024-10-23 12:41:01 +0300

    finished Aufgabe3 again(1)

--- src/Aufgabe3.java
+++ src/Aufgabe3.java
@@ -103,6 +103,7 @@ public class Aufgabe3 {
         //1
         int[] s1={1,3,0,0,0,0,0,0,0},s2={8,7,0,0,0,0,0,0,0};
         int[] result1 = summe(s1,s2);
+        System.out.println("Die Summe Array ist: ");
         for(int i=0; i<result1.length; i++) {
             System.out.print(result1[i]+" ");
         }
@@ -111,6 +112,7 @@ public class Aufgabe3 {
         //2
         int[] d1={8,3,0,0,0,0,0,0,0},d2={5,4,0,0,0,0,0,0,0};
         int[] result2 = differenz(d1,d2);
+        System.out.println("Die Differenz Array ist: ");
         for(int i=0; i<result2.length; i++) {
             System.out.print(result2[i]+" ");
         }
@@ -119,6 +121,7 @@ public class Aufgabe3 {
         //3
         int[] p1={2,3,6,0,0,0,0,0,0};
         int[] result3 = produkt(p1,2);
+        System.out.println("Die Produkt Array ist: ");
         for(int i=0; i<result3.length; i++) {
             System.out.print(result3[i]+" ");
         }
@@ -127,6 +130,7 @@ public class Aufgabe3 {
         //4
         int[] div1={2,3,6,0,0,0,0,0,0};
         int[] result4 = division(div1,2);
+        System.out.println("Die Divisions Array ist: ");
         for(int i=0; i<result4.length; i++) {
             System.out.print(result4[i]+" ");
         }
